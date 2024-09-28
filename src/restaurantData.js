7f6704d7600bc32d550b53630ba999adda899bf1
zouhelen <helenzou05@gmail.com>
2024-09-28 15:46:04 -0400

    restaurant data added

--- src/restaurantData.js
+++ src/restaurantData.js
@@ -167,6 +167,86 @@ const gnvFood = [
       types: "fast food",
       cuisine: "american",
       price: 1
+  }, {
+    name: "Satchel's Pizza",
+    distance: 4.4,
+    openTime: 11.0,
+    closeTime: 22.0,
+    types: "gourmet",
+    cuisine: "european",
+    price: 1
+  }, {
+    name: "Olive Garden",
+    distance: 3.9,
+    openTime: 11.0,
+    closeTime: 23.0,
+    types: "gourmet",
+    cuisine: "european",
+    price: 1
+  }, {
+    name: "Cheesecake Factory",
+    distance: 2.9,
+    openTime: 11.0,
+    closeTime: 21.0,
+    types: "gourmet",
+    cuisine: "european",
+    price: 2
+  }, {
+    name: "Culver's",
+    distance: 4.0,
+    openTime: 10.0,
+    closeTime: 24.0,
+    types: "fast food",
+    cuisine: "american",
+    price: 1
+  }, {
+    name: "Red Rice",
+    distance: 3.1,
+    openTime: 11.5,
+    closeTime: 21.0,
+    types: "gourmet",
+    cuisine: "asian",
+    price: 1
+  }, {
+    name: "Waffle House",
+    distance: 3.7,
+    openTime: 0.0,
+    closeTime: 24.0,
+    types: "fast food",
+    cuisine: "american",
+    price: 1
+  }, {
+    name: "Ocean Buffet",
+    distance: 4.7,
+    openTime: 11.0,
+    closeTime: 22.5,
+    types: "buffet",
+    cuisine: "asian",
+    price: 1
+  }, {
+    name: "Gator Corner",
+    distance: 0.4,
+    openTime: 7.0,
+    closeTime: 21.0,
+    types: "buffet",
+    cuisine: "american",
+    price: 1
+  }, {
+    name: "Mi Apa",
+    distance: 0.5,
+    openTime: 7.0,
+    closeTime: 17.0,
+    types: "gourmet",
+    cuisine: "hispanic",
+    price: 1
+  }, {
+    name: "Bolay",
+    distance: 0.5,
+    openTime: 11.0,
+    closeTime: 21.0,
+    types: "fast food",
+    cuisine: "american",
+    price: 1
   }
 ]
 
