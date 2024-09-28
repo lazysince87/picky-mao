c0b4b3a6b178e871c0211c48e16a9d9736e200bf
yam-online <waterlilypearl@gmail.com>
2024-09-28 00:39:27 -0400

    added google.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import PlaceSearch from "./google.js";
 
 export default function Home() {
   return (
@@ -12,9 +13,10 @@ export default function Home() {
           height={38}
           priority
         />
+        <div><PlaceSearch /></div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            MAGGIEYAN{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.js
             </code>
